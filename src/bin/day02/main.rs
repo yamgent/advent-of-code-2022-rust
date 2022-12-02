b6792ffdc387ae5ff08259a34a7d1ae2b06497bf
Maurice Wangleng Tan <tanwangleng@outlook.com>
2022-12-02 21:14:11 +0800

    Clean up day 02

--- src/bin/day02/main.rs
+++ src/bin/day02/main.rs
@@ -1,6 +1,6 @@
 const ACTUAL_INPUT: &str = include_str!("./input.txt");
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 enum Shape {
     Rock,
     Paper,
@@ -14,6 +14,22 @@ impl Shape {
             "B" | "Y" => Self::Paper,
             "C" | "Z" => Self::Scissors,
             _ => panic!("{} is not a valid shape", shape),
+        }
+    }
+
+    fn get_correct_shape(opponent: &Shape, outcome: &Outcome) -> Shape {
+        match outcome {
+            Outcome::Tie => opponent.clone(),
+            Outcome::Win => match opponent {
+                Shape::Rock => Shape::Paper,
+                Shape::Paper => Shape::Scissors,
+                Shape::Scissors => Shape::Rock,
+            },
+            Outcome::Lose => match opponent {
+                Shape::Rock => Shape::Scissors,
+                Shape::Paper => Shape::Rock,
+                Shape::Scissors => Shape::Paper,
+            },
         }
     }
 }
@@ -33,25 +49,44 @@ impl Outcome {
             _ => panic!("{} is not a valid outcome", outcome),
         }
     }
+
+    fn get_outcome(opponent: &Shape, me: &Shape) -> Outcome {
+        match (opponent, me) {
+            (Shape::Rock, Shape::Paper)
+            | (Shape::Paper, Shape::Scissors)
+            | (Shape::Scissors, Shape::Rock) => Outcome::Win,
+            (Shape::Rock, Shape::Rock)
+            | (Shape::Paper, Shape::Paper)
+            | (Shape::Scissors, Shape::Scissors) => Outcome::Tie,
+            _ => Outcome::Lose,
+        }
+    }
 }
 
-struct Play(Shape, Shape);
+struct Play {
+    me: Shape,
+    outcome: Outcome,
+}
 
 impl Play {
     fn parse_p1(line: &str) -> Self {
-        let mut parts = line.split(" ").map(Shape::parse);
+        let parts = line.split(" ").map(Shape::parse).collect::<Vec<_>>();
 
-        let opponent = parts
-            .next()
-            .unwrap_or_else(|| panic!("Expected two parts, found nothing for {}", line));
-        let me = parts
-            .next()
-            .unwrap_or_else(|| panic!("Expected two parts, only found one for {}", line));
-        if parts.count() > 0 {
-            panic!("Expected two parts, found more than two for {}", line);
+        if parts.len() != 2 {
+            panic!(
+                "Expected two parts, found {} parts for {}",
+                parts.len(),
+                line
+            );
         }
 
-        Self(opponent, me)
+        let opponent = parts[0];
+        let me = parts[1];
+
+        Self {
+            outcome: Outcome::get_outcome(&opponent, &me),
+            me,
+        }
     }
 
     fn parse_p2(line: &str) -> Self {
@@ -63,46 +98,25 @@ impl Play {
                 .unwrap_or_else(|| panic!("Expected two parts, found nothing for {}", line)),
         );
 
-        let me = match Outcome::parse(
+        let outcome = Outcome::parse(
             parts
                 .next()
                 .unwrap_or_else(|| panic!("Expected two parts, only found one for {}", line)),
-        ) {
-            Outcome::Tie => opponent.clone(),
-            Outcome::Win => match opponent {
-                Shape::Rock => Shape::Paper,
-                Shape::Paper => Shape::Scissors,
-                Shape::Scissors => Shape::Rock,
-            },
-            Outcome::Lose => match opponent {
-                Shape::Rock => Shape::Scissors,
-                Shape::Paper => Shape::Rock,
-                Shape::Scissors => Shape::Paper,
-            },
-        };
+        );
 
-        Self(opponent, me)
-    }
-
-    fn get_outcome(&self) -> Outcome {
-        match self {
-            Play(Shape::Rock, Shape::Paper)
-            | Play(Shape::Paper, Shape::Scissors)
-            | Play(Shape::Scissors, Shape::Rock) => Outcome::Win,
-            Play(Shape::Rock, Shape::Rock)
-            | Play(Shape::Paper, Shape::Paper)
-            | Play(Shape::Scissors, Shape::Scissors) => Outcome::Tie,
-            _ => Outcome::Lose,
+        Self {
+            me: Shape::get_correct_shape(&opponent, &outcome),
+            outcome,
         }
     }
 
     fn get_score(&self) -> i32 {
-        let outcome_score = match self.get_outcome() {
+        let outcome_score = match self.outcome {
             Outcome::Win => 6,
             Outcome::Tie => 3,
             Outcome::Lose => 0,
         };
-        let shape_score = match self.1 {
+        let shape_score = match self.me {
             Shape::Rock => 1,
             Shape::Paper => 2,
             Shape::Scissors => 3,
