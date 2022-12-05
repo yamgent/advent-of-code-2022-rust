d233b35f756f06e7dc710477ac90eb2caad2d310
Maurice Wangleng Tan <tanwangleng@outlook.com>
2022-12-05 21:50:23 +0800

    Improve day 05

--- src/bin/day05/main.rs
+++ src/bin/day05/main.rs
@@ -1,5 +1,6 @@
+use itertools::Itertools;
+
 const ACTUAL_INPUT: &str = include_str!("./input.txt");
-const ACTUAL_META: (usize, usize) = (9, 8);
 
 struct TestCase {
     stacks: Vec<Vec<char>>,
@@ -13,26 +14,39 @@ struct Instruction {
 }
 
 impl TestCase {
-    fn parse_input(input: &str, meta: &(usize, usize)) -> Self {
-        let mut stacks = std::iter::repeat(vec![]).take(meta.0).collect::<Vec<_>>();
+    fn parse_input(input: &str) -> Self {
+        let (total_height, floor_line) = input
+            .trim()
+            .lines()
+            .enumerate()
+            .find(|(_, line)| line.trim().starts_with('1'))
+            .unwrap();
+
+        let total_stacks = floor_line
+            .split_whitespace()
+            .last()
+            .unwrap()
+            .parse::<usize>()
+            .unwrap();
+
+        let mut stacks = std::iter::repeat(vec![])
+            .take(total_stacks)
+            .collect::<Vec<_>>();
 
         input
             .lines()
-            .take(meta.1)
+            .take(total_height)
             .collect::<Vec<_>>()
             .into_iter()
             .rev()
             .for_each(|line| {
-                let mut line = line.chars();
-                stacks.iter_mut().for_each(|stack| {
-                    let character = line.nth(1).unwrap();
-                    line.next();
-                    line.next();
-
-                    if character != ' ' {
-                        stack.push(character);
-                    }
-                });
+                line.chars()
+                    .chunks(4)
+                    .into_iter()
+                    .map(|mut s| s.nth(1).unwrap())
+                    .zip(stacks.iter_mut())
+                    .filter(|(character, _)| *character != ' ')
+                    .for_each(|(character, stack)| stack.push(character));
             });
 
         Self {
@@ -40,7 +54,7 @@ impl TestCase {
             instructions: input
                 .trim()
                 .lines()
-                .skip(meta.1 + 2)
+                .skip(total_height + 2)
                 .map(|line| {
                     let mut parts = line.split_whitespace();
 
@@ -55,11 +69,18 @@ impl TestCase {
     }
 }
 
-fn p1(input: &str, meta: &(usize, usize)) -> String {
+fn transform_to_result(stacks: Vec<Vec<char>>) -> String {
+    stacks
+        .into_iter()
+        .map(|mut stack| stack.pop().unwrap())
+        .collect()
+}
+
+fn p1(input: &str) -> String {
     let TestCase {
         mut stacks,
         instructions,
-    } = TestCase::parse_input(input, meta);
+    } = TestCase::parse_input(input);
 
     instructions.into_iter().for_each(
         |Instruction {
@@ -74,17 +95,14 @@ fn p1(input: &str, meta: &(usize, usize)) -> String {
         },
     );
 
-    stacks
-        .into_iter()
-        .map(|mut stack| stack.pop().unwrap())
-        .collect()
+    transform_to_result(stacks)
 }
 
-fn p2(input: &str, meta: &(usize, usize)) -> String {
+fn p2(input: &str) -> String {
     let TestCase {
         mut stacks,
         instructions,
-    } = TestCase::parse_input(input, meta);
+    } = TestCase::parse_input(input);
 
     instructions.into_iter().for_each(
         |Instruction {
@@ -98,15 +116,12 @@ fn p2(input: &str, meta: &(usize, usize)) -> String {
         },
     );
 
-    stacks
-        .into_iter()
-        .map(|mut stack| stack.pop().unwrap())
-        .collect()
+    transform_to_result(stacks)
 }
 
 fn main() {
-    println!("{}", p1(ACTUAL_INPUT, &ACTUAL_META));
-    println!("{}", p2(ACTUAL_INPUT, &ACTUAL_META));
+    println!("{}", p1(ACTUAL_INPUT));
+    println!("{}", p2(ACTUAL_INPUT));
 }
 
 #[cfg(test)]
@@ -114,25 +129,24 @@ mod tests {
     use super::*;
 
     const SAMPLE_INPUT: &str = include_str!("./sample.txt");
-    const SAMPLE_META: (usize, usize) = (3, 3);
 
     #[test]
     fn test_p1_sample() {
-        assert_eq!(p1(SAMPLE_INPUT, &SAMPLE_META), "CMZ");
+        assert_eq!(p1(SAMPLE_INPUT), "CMZ");
     }
 
     #[test]
     fn test_p1_actual() {
-        assert_eq!(p1(ACTUAL_INPUT, &ACTUAL_META), "GFTNRBZPF");
+        assert_eq!(p1(ACTUAL_INPUT), "GFTNRBZPF");
     }
 
     #[test]
     fn test_p2_sample() {
-        assert_eq!(p2(SAMPLE_INPUT, &SAMPLE_META), "MCD");
+        assert_eq!(p2(SAMPLE_INPUT), "MCD");
     }
 
     #[test]
     fn test_p2_actual() {
-        assert_eq!(p2(ACTUAL_INPUT, &ACTUAL_META), "VRQWPDSGP");
+        assert_eq!(p2(ACTUAL_INPUT), "VRQWPDSGP");
     }
 }
