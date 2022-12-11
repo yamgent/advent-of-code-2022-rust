7213701702d44a098d587e1625e59f6eda44c1de
Maurice Wangleng Tan <tanwangleng@outlook.com>
2022-12-11 14:50:10 +0800

    Solve day 11 part 02

--- src/bin/day11/main.rs
+++ src/bin/day11/main.rs
@@ -155,8 +155,41 @@ fn p1(input: &str) -> String {
 }
 
 fn p2(input: &str) -> String {
-    let _input = input.trim();
-    "".to_string()
+    let mut monkeys = Monkey::parse_input(input);
+    let prime = monkeys.iter().map(|monkey| monkey.test).product::<i64>();
+
+    (0..10000).for_each(|_| {
+        (0..monkeys.len()).for_each(|i| {
+            let monkey = &mut monkeys[i];
+            let destinations = monkey
+                .items
+                .iter()
+                .map(|val| {
+                    let val = monkey.op.compute(val) % prime;
+                    let target = if val % monkey.test == 0 {
+                        monkey.throw_true
+                    } else {
+                        monkey.throw_false
+                    };
+                    (target, val)
+                })
+                .collect::<Vec<_>>();
+            monkey.inspected += monkey.items.len();
+            monkey.items.clear();
+
+            destinations.into_iter().for_each(|(target, val)| {
+                monkeys[target].items.push(val);
+            });
+        });
+    });
+
+    monkeys.sort_by(|a, b| a.inspected.cmp(&b.inspected).reverse());
+    monkeys
+        .into_iter()
+        .take(2)
+        .map(|monkey| monkey.inspected)
+        .product::<usize>()
+        .to_string()
 }
 
 fn main() {
@@ -182,12 +215,11 @@ mod tests {
 
     #[test]
     fn test_p2_sample() {
-        assert_eq!(p2(SAMPLE_INPUT), "");
+        assert_eq!(p2(SAMPLE_INPUT), "2713310158");
     }
 
     #[test]
-    #[ignore = "not yet implemented"]
     fn test_p2_actual() {
-        assert_eq!(p2(ACTUAL_INPUT), "");
+        assert_eq!(p2(ACTUAL_INPUT), "25935263541");
     }
 }
